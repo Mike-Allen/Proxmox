bbdae5164698b3d8249f08ede8be31a25cb6883a
Mike-Allen <1113284+Mike-Allen@users.noreply.github.com>
2025-02-01 13:53:33 -0800

    Update paperless-ngx.sh - Added a hello message
    When the update finished a hello message now displayed.

--- ct/paperless-ngx.sh
+++ ct/paperless-ngx.sh
@@ -122,6 +122,6 @@ start
 build_container
 description
 
-msg_ok "Completed Successfully!\n"
+msg_ok "Completed Successfully! Mike says hi!\n"
 echo -e "${APP} should be reachable by going to the following URL.
          ${BL}http://${IP}:8000${CL} \n"
