f3311df066800038bf6f8bb8767e803f278b26a1
Mike-Allen <1113284+Mike-Allen@users.noreply.github.com>
2025-02-01 16:21:39 -0800

    Change links to use the Localized branch files instead of the main branch.

--- install/fenrus-install.sh
+++ install/fenrus-install.sh
@@ -4,7 +4,7 @@
 # Author: tteck (tteckster)
 # Co-Author: Scorpoon
 # License: MIT
-# https://github.com/tteck/Proxmox/raw/main/LICENSE
+# https://github.com/Mike-Allen/Proxmox/raw/Localize/LICENSE
 # Source: https://github.com/revenz/Fenrus
 
 
