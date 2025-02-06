aa6798ddb3bbe3e3c9499ed7dc97b6f9e20cfa8a
Mike-Allen <1113284+Mike-Allen@users.noreply.github.com>
2025-02-06 14:22:54 -0800

    Debugging

--- ct/create_lxc.sh
+++ ct/create_lxc.sh
@@ -178,10 +178,11 @@ PCT_OPTIONS=(${PCT_OPTIONS[@]:-${DEFAULT_PCT_OPTIONS[@]}})
 
 # Create container
 msg_info "Creating LXC Container"
-msg_info "CT ID            = "+=$CTID
-msg_info "TEMPLATE_STORAGE = "+=${TEMPLATE_STORAGE}
-msg_info "TEMPLATE         = "+=${TEMPLATE}
-msg_info "PCT_OPTIONS      = "+=${PCT_OPTIONS}
+msg_info "\nCT ID            = "+=$CTID
+msg_info "\nTEMPLATE_STORAGE = "+=${TEMPLATE_STORAGE}
+msg_info "\nTEMPLATE         = "+=${TEMPLATE}
+msg_info "\nPCT_OPTIONS      = "+=${PCT_OPTIONS}
+msg_info "End of Debugging\n"
 pct create $CTID ${TEMPLATE_STORAGE}:vztmpl/${TEMPLATE} ${PCT_OPTIONS[@]} >/dev/null ||
   exit "A problem occured while trying to create container."
 msg_ok "LXC Container ${BL}$CTID${CL} ${GN}was successfully created."
