ee852aa2ca640e21561460b7cb6458b9b5a6e936
Mike-Allen <1113284+Mike-Allen@users.noreply.github.com>
2026-01-20 20:41:05 -0800

    Modify pip install command to include --break-system-packages
    Allow the packages to be installed in the system and ignore the recommended virtual environment.

--- ct/paperless-ngx.sh
+++ ct/paperless-ngx.sh
@@ -90,7 +90,7 @@ function update_script() {
       cp -r /opt/paperless/paperless.conf paperless-ngx/
       cp -r paperless-ngx/* /opt/paperless/
       cd /opt/paperless
-      pip install -r requirements.txt # &>/dev/null
+      pip install -r --break-system-packages requirements.txt # &>/dev/null
       cd /opt/paperless/src
       /usr/bin/python3 manage.py migrate &>/dev/null
       echo "${RELEASE}" >/opt/${APP}_version.txt
